e55b686054dadc33cef0186822aab8bcc20a7f7a
Pon Cheol Ku (구본철) <bbon@live.com>
2021-11-23 12:53:03 +0900

    Initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Example for next-i18next
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    
+    return (
+        <div>
+            <h1>Hello world!</h1>
+        </div>
+
+    );
+};
+
+export default HomePage;
