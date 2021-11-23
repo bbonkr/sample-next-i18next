ff47aeb66fee6454e6d1fa1b3ba514b95bf3e50a
Pon Cheol Ku (구본철) <bbon@live.com>
2021-11-23 15:14:13 +0900

    Verify to navigate using useRouter #5

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Layout from '../components/common/Layout';
 
 const AboutPage = () => {
     const { t } = useTranslation();
+    const router = useRouter();
+
+    const handleClickNavigate = useCallback(() => {
+        router.push('/');
+    }, []);
+
     return (
-        <div>
+        <Layout>
             <h1>{t('about.title')}</h1>
             <hr />
             <div>
                 <Link href="/">
                     <a>{t('index.title')}</a>
                 </Link>
+                <span> | </span>
+                <button onClick={handleClickNavigate}>
+                    {t('index.title')}
+                </button>
             </div>
-        </div>
+        </Layout>
     );
 };
 
